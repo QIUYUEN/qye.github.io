beda60d41bcd80a3aeffb4e87722b64ea7d37b17
QYE <49049061+QIUYUEN@users.noreply.github.com>
2025-07-17 08:52:40 +0800

    Add files via upload

---
+++ js/logosearch.js
@@ -0,0 +1,612 @@
+const softwareData = [
+
+  // LOGO
+
+  {
+    name: "上海开放大学LOGO",
+    file: "上海开放大学LOGO.ai",
+    description: "上海开放大学LOGO",
+    page: "logo1.html",
+    thumbnail: "images/logo/logo1/logo_2.png",
+  },
+
+  {
+    name: "上海学习网LOGO",
+    file: "上海开放大学LOGO.ai",
+    description: "上海学习网LOGO",
+    page: "logo2.html",
+    thumbnail: "images/logo/logo2/logo_1.png",
+  },
+
+  {
+    name: "上海政府LOGO",
+    file: "上海市政府logo.ai",
+    description: "上海政府LOGO",
+    page: "logo3.html",
+    thumbnail: "images/logo/logo3/logo_1.png",
+  },
+
+  {
+    name: "中德开放在线学习平台LOGO",
+    file: "中德开放在线学习平台logo.ai",
+    description: "中德开放在线学习平台LOGO",
+    page: "logo4.html",
+    thumbnail: "images/logo/logo4/logo_1.png",
+  },
+
+  {
+    name: "上海市终身教育学分银行LOGO",
+    file: "上海市终身教育学分银行logo.ai",
+    description: "上海市终身教育学分银行LOGO",
+    page: "logo5.html",
+    thumbnail: "images/logo/logo5/logo_1.png",
+  },
+
+  {
+    name: "上海教育软件发展有限公司LOGO",
+    file: "上海教育软件发展有限公司logo.ai",
+    description: "上海教育软件发展有限公司LOGO",
+    page: "logo6.html",
+    thumbnail: "images/logo/logo6/logo_1.png",
+  },
+
+  {
+    name: "上海教育软件发展有限公司旧版LOGO",
+    file: "上海教育软件发展有限公司旧版LOGO.ai",
+    description: "上海教育软件发展有限公司旧版LOGO",
+    page: "logo7.html",
+    thumbnail: "images/logo/logo7/logo_1.png",
+  },
+
+  {
+    name: "市民终身学习周LOGO",
+    file: "市民终身学习周logo.ai",
+    description: "市民终身学习周LOGO",
+    page: "logo8.html",
+    thumbnail: "images/logo/logo8/logo_1.png",
+  },
+
+  {
+    name: "银发e学堂LOGO",
+    file: "银发e学堂logo.ai",
+    description: "银发e学堂LOGO",
+    page: "logo9.html",
+    thumbnail: "images/logo/logo9/logo_1.png",
+  },
+
+  {
+    name: "民生学院LOGO",
+    file: "民生学院logo.ai",
+    description: "民生学院LOGO",
+    page: "logo10.html",
+    thumbnail: "images/logo/logo10/logo_1.png",
+  },
+
+  {
+    name: "上海老年教育慕课LOGO",
+    file: "上海老年教育慕课logo.ai",
+    description: "上海老年教育慕课LOGO",
+    page: "logo11.html",
+    thumbnail: "images/logo/logo11/logo_1.png",
+  },
+
+  {
+    name: "上海微校LOGO",
+    file: "上海微校logo.ai",
+    description: "上海微校LOGO",
+    page: "logo12.html",
+    thumbnail: "images/logo/logo12/logo_1.png",
+  },
+
+  {
+    name: "黄浦老年教育LOGO",
+    file: "黄浦老年教育logo.ai",
+    description: "黄浦老年教育LOGO",
+    page: "logo13.html",
+    thumbnail: "images/logo/logo13/logo_1.png",
+  },
+
+  {
+    name: "上海市民终身学习体验基地LOGO",
+    file: "上海市民终身学习体验基地logo.ai",
+    description: "上海市民终身学习体验基地LOGO",
+    page: "logo14.html",
+    thumbnail: "images/logo/logo14/logo_1.png",
+  },
+
+  {
+    name: "上海市教师教育学院LOGO",
+    file: "上海市教师教育学院logo.ai",
+    description: "上海市教师教育学院LOGO",
+    page: "logo15.html",
+    thumbnail: "images/logo/logo15/logo_2.png",
+  },
+
+  {
+    name: "上海市学习型社会建设服务指导中心LOGO",
+    file: "上海市学习型社会建设服务指导中心logo.ai",
+    description: "上海市学习型社会建设服务指导中心LOGO",
+    page: "logo16.html",
+    thumbnail: "images/logo/logo16/logo_1.png",
+  },
+
+  {
+    name: "学在黄浦LOGO",
+    file: "学在黄浦logo.ai",
+    description: "学在黄浦LOGO",
+    page: "logo17.html",
+    thumbnail: "images/logo/logo17/logo_1.png",
+  },
+
+  {
+    name: "闵行学习LOGO",
+    file: "闵行学习logo.ai",
+    description: "闵行学习LOGO",
+    page: "logo18.html",
+    thumbnail: "images/logo/logo18/logo_1.png",
+  },
+
+  {
+    name: "普陀学习LOGO",
+    file: "普陀学习logo.ai",
+    description: "普陀学习LOGOO",
+    page: "logo19.html",
+    thumbnail: "images/logo/logo19/logo_1.png",
+  },
+
+  {
+    name: "长三角职业教育一体化协同发展联盟LOGO",
+    file: "长三角职业教育一体化协同发展联盟logo.ai",
+    description: "长三角职业教育一体化协同发展联盟LOGO",
+    page: "logo20.html",
+    thumbnail: "images/logo/logo20/logo_1.png",
+  },
+
+  {
+    name: "2024闵行区市民读书活动LOGO",
+    file: "2024闵行区市民读书活动logo.ai",
+    description: "2024闵行区市民读书活动LOGO",
+    page: "logo21.html",
+    thumbnail: "images/logo/logo21/logo_1.png",
+  },
+
+  {
+    name: "汇地图LOGO",
+    file: "汇地图logo.ai",
+    description: "汇地图LOGO",
+    page: "logo22.html",
+    thumbnail: "images/logo/logo22/logo_1.png",
+  },
+
+  {
+    name: "社区教育云视课堂LOGO",
+    file: "社区教育云视课堂logo.ai",
+    description: "社区教育云视课堂LOGO",
+    page: "logo23.html",
+    thumbnail: "images/logo/logo23/logo_1.png",
+  },
+
+  {
+    name: "上海教育资源中心LOGO",
+    file: "上海教育资源中心logo.ai",
+    description: "上海教育资源中心LOGO",
+    page: "logo24.html",
+    thumbnail: "images/logo/logo24/logo_1.png",
+  },
+
+  {
+    name: "学习，遇到更好的自己LOGO",
+    file: "学习遇到更好的自己logo.psd",
+    description: "学习，遇到更好的自己LOGO",
+    page: "logo25.html",
+    thumbnail: "images/logo/logo25/logo_1.png",
+  },
+
+  {
+    name: "巾帼大学堂LOGO",
+    file: "巾帼大学堂logo.ai",
+    description: "巾帼大学堂LOGO",
+    page: "logo26.html",
+    thumbnail: "images/logo/logo26/logo_1.png",
+  },
+
+  {
+    name: "学习坊LOGO",
+    file: "学习坊logo.ai",
+    description: "学习坊LOGO",
+    page: "logo27.html",
+    thumbnail: "images/logo/logo27/logo_1.png",
+  },
+
+  {
+    name: "多语言在线学习平台LOGO学习坊LOGO",
+    file: "多语言在线学习平台logo.ai",
+    description: "多语言在线学习平台LOGO",
+    page: "logo28.html",
+    thumbnail: "images/logo/logo28/logo_1.png",
+  },
+
+  {
+    name: "上海市电视中等专业学校LOGO",
+    file: "上海市电视中等专业学校logo.ai",
+    description: "上海市电视中等专业学校LOGO",
+    page: "logo29.html",
+    thumbnail: "images/logo/logo29/logo_1.png",
+  },
+
+  {
+    name: "上海电达信息技术有限公司LOGO",
+    file: "上海电达信息技术有限公司logo.ai",
+    description: "上海电达信息技术有限公司LOGO",
+    page: "logo30.html",
+    thumbnail: "images/logo/logo30/logo_1.png",
+  },
+
+  {
+    name: "上海市中小学专题教育LOGO",
+    file: "上海市中小学专题教育logo.ai",
+    description: "上海市中小学专题教育LOGO",
+    page: "logo31.html",
+    thumbnail: "images/logo/logo31/logo_1.png",
+  },
+
+  {
+    name: "上海市学生综合素质评价信息管理系统LOGO",
+    file: "上海学生综评logo.ai",
+    description: "上海市学生综合素质评价信息管理系统LOGO",
+    page: "logo32.html",
+    thumbnail: "images/logo/logo32/logo_1.png",
+  },
+
+  {
+    name: "上海教育电视台LOGO",
+    file: "上海教育电视台logo.ai",
+    description: "上海教育电视台LOGO",
+    page: "logo33.html",
+    thumbnail: "images/logo/logo33/logo_1.png",
+  },
+
+  {
+    name: "上海市教师教育网LOGO",
+    file: "上海市教师教育网logo.ai",
+    description: "上海市教师教育网LOGO",
+    page: "logo34.html",
+    thumbnail: "images/logo/logo34/logo_1.png",
+  },
+
+  {
+    name: "上海市教育委员会教育技术装备中心LOGO",
+    file: "上海市教育委员会教育技术装备中心logo.ai",
+    description: "上海市教育委员会教育技术装备中心LOGO",
+    page: "logo35.html",
+    thumbnail: "images/logo/logo35/logo_1.png",
+  },
+
+  // 网页
+
+  {
+    name: "上海教育电视台网站",
+    file: "上海教育电视台网站.psd",
+    description: "上海教育电视台网站",
+    page: "website1.html",
+    thumbnail: "images/website/website1/website_1.avif",
+  },
+
+  {
+    name: "上海教育资源中心网站",
+    file: "上海教育资源中心网站.psd",
+    description: "上海教育资源中心网站",
+    page: "website2.html",
+    thumbnail: "images/website/website2/website_1.avif",
+  },
+
+  {
+    name: "云南社区老年教育网站",
+    file: "云南社区老年教育网站.psd",
+    description: "云南社区老年教育网站",
+    page: "website3.html",
+    thumbnail: "images/website/website3/website_1.avif",
+  },
+
+  {
+    name: "全国社区服饰大赛网站",
+    file: "全国社区服饰大赛网站.psd",
+    description: "全国社区服饰大赛网站",
+    page: "website4.html",
+    thumbnail: "images/website/website4/website_1.avif",
+  },
+
+  {
+    name: "网上老年学堂网站",
+    file: "网上老年学堂网站.psd",
+    description: "网上老年学堂网站",
+    page: "website5.html",
+    thumbnail: "images/website/website5/website_1.avif",
+  },
+
+  {
+    name: "光影临港红色瞬间网站",
+    file: "光影临港红色瞬间网站.psd",
+    description: "光影临港红色瞬间网站",
+    page: "website6.html",
+    thumbnail: "images/website/website6/website_1.avif",
+  },
+
+  {
+    name: "学在数字长宁网站",
+    file: "学在数字长宁网站.psd",
+    description: "学在数字长宁网站",
+    page: "website7.html",
+    thumbnail: "images/website/website7/website_1.avif",
+  },
+
+  {
+    name: "名师工作室网站",
+    file: "名师工作室网站.psd",
+    description: "名师工作室网站",
+    page: "website8.html",
+    thumbnail: "images/website/website8/website_1.avif",
+  },
+
+  {
+    name: "上海党员干部现代远程教育互联网平台网站",
+    file: "上海党员干部现代远程教育互联网平台网站.psd",
+    description: "上海党员干部现代远程教育互联网平台网站",
+    page: "website9.html",
+    thumbnail: "images/website/website9/website_1.avif",
+  },
+
+  {
+    name: "上海市教育科学研究院网站",
+    file: "上海市教育科学研究院网站.psd",
+    description: "上海市教育科学研究院网站",
+    page: "website10.html",
+    thumbnail: "images/website/website10/website_1.avif",
+  },
+
+  {
+    name: "上海市教育科学研究院网站",
+    file: "上海市教育科学研究院网站.psd",
+    description: "上海市教育科学研究院网站",
+    page: "website11.html",
+    thumbnail: "images/website/website11/website_1.avif",
+  },
+
+  {
+    name: "上海开放大学网站",
+    file: "上海开放大学网站.psd",
+    description: "上海开放大学网站",
+    page: "website12.html",
+    thumbnail: "images/website/website12/website_1.avif",
+  },
+
+  {
+    name: "上海开放大学课程开放平台网站",
+    file: "上海开放大学课程开放平台网站.psd",
+    description: "上海开放大学课程开放平台网站",
+    page: "website13.html",
+    thumbnail: "images/website/website13/website_1.avif",
+  },
+
+  {
+    name: "上海开放大学60周年网站",
+    file: "上海开放大学60周年网站.psd",
+    description: "上海开放大学60周年网站",
+    page: "website14.html",
+    thumbnail: "images/website/website14/website_1.avif",
+  },
+
+  {
+    name: "上海职业院校教师企业实践平台网站",
+    file: "上海职业院校教师企业实践平台网站.psd",
+    description: "上海职业院校教师企业实践平台网站",
+    page: "website15.html",
+    thumbnail: "images/website/website15/website_1.avif",
+  },
+
+  {
+    name: "上海科技节网站",
+    file: "上海科技节网站.psd",
+    description: "上海科技节网站",
+    page: "website16.html",
+    thumbnail: "images/website/website16/website_1.avif",
+  },
+
+  {
+    name: "上海国际电影节网站",
+    file: "上海国际电影节网站.psd",
+    description: "上海国际电影节网站",
+    page: "website17.html",
+    thumbnail: "images/website/website17/website_1.avif",
+  },
+
+  {
+    name: "中德开放在线学习平台网站",
+    file: "中德开放在线学习平台网站.psd",
+    description: "中德开放在线学习平台网站",
+    page: "website18.html",
+    thumbnail: "images/website/website18/website_1.avif",
+  },
+
+  {
+    name: "中国福利会幼儿园网站",
+    file: "中国福利会幼儿园网站.psd",
+    description: "中国福利会幼儿园网站",
+    page: "website19.html",
+    thumbnail: "images/website/website19/website_1.avif",
+  },
+
+  {
+    name: "上海开放大学非学历教育学习平台网站",
+    file: "上海开放大学非学历教育学习平台网站.psd",
+    description: "上海开放大学非学历教育学习平台网站",
+    page: "website20.html",
+    thumbnail: "images/website/website20/website_1.avif",
+  },
+
+  {
+    name: "上海开放大学崇明分校网站",
+    file: "上海开放大学崇明分校网站.psd",
+    description: "上海开放大学崇明分校网站",
+    page: "website21.html",
+    thumbnail: "images/website/website21/website_1.avif",
+  },
+
+  {
+    name: "人工智能学习平台网站",
+    file: "人工智能学习平台网站.psd",
+    description: "人工智能学习平台网站",
+    page: "website22.html",
+    thumbnail: "images/website/website22/website_1.avif",
+  },
+
+  {
+    name: "上海教育资源中心网站",
+    file: "上海教育资源中心网站.psd",
+    description: "上海教育资源中心网站",
+    page: "website23.html",
+    thumbnail: "images/website/website23/website_1.avif",
+  },
+
+  {
+    name: "黄浦区人工智能教育平台网站",
+    file: "黄浦区人工智能教育平台网站.psd",
+    description: "黄浦区人工智能教育平台网站",
+    page: "website24.html",
+    thumbnail: "images/website/website24/website_1.avif",
+  },
+
+  {
+    name: "美术团队资源库网站",
+    file: "美术团队资源库网站.psd",
+    description: "美术团队资源库网站",
+    page: "website25.html",
+    thumbnail: "images/website/website25/website_1.avif",
+  },
+
+  {
+    name: "上海微校上海学习网网站",
+    file: "上海微校上海学习网网站.psd",
+    description: "上海微校上海学习网网站",
+    page: "website26.html",
+    thumbnail: "images/website/website26/website_1.avif",
+  },
+
+  {
+    name: "多语言在线学习平台网站",
+    file: "多语言在线学习平台网站.psd",
+    description: "多语言在线学习平台网站",
+    page: "website27.html",
+    thumbnail: "images/website/website27/website_1.avif",
+  },
+
+  {
+    name: "上海党员干部现代远程教育互联网平台网站",
+    file: "上海党员干部现代远程教育互联网平台网站.psd",
+    description: "上海党员干部现代远程教育互联网平台网站",
+    page: "website28.html",
+    thumbnail: "images/website/website28/website_1.avif",
+  },
+
+  {
+    name: "上海学习型社会建设网网站",
+    file: "上海学习型社会建设网网站.psd",
+    description: "上海学习型社会建设网网站",
+    page: "website29.html",
+    thumbnail: "images/website/website29/website_1.avif",
+  },
+
+  {
+    name: "上海教育软件发展有限公司网站",
+    file: "上海教育软件发展有限公司网站.psd",
+    description: "上海教育软件发展有限公司网站",
+    page: "website30.html",
+    thumbnail: "images/website/website30/website_1.avif",
+  },
+
+];
+
+// 首页搜索框的处理
+function searchSoftware() {
+  const query = document.getElementById("searchInput")?.value.trim();
+  if (query) {
+    window.location.href = `results.html?query=${encodeURIComponent(query)}`;
+  }
+}
+
+// 搜索结果页面的处理
+function handleSearchResults() {
+  const query = new URLSearchParams(window.location.search).get("query");
+  if (query) {
+    const filteredSoftware = softwareData.filter((software) =>
+      software.name.toLowerCase().includes(query.toLowerCase())
+    );
+    displaySoftwareList(filteredSoftware);
+  }
+}
+
+function displaySoftwareList(softwareList) {
+  const listElement = document.getElementById("softwareList");
+  if (!listElement) return; // 防止非搜索结果页出错
+
+  listElement.innerHTML = "";
+
+  if (softwareList.length === 0) {
+    listElement.innerHTML = "<li>😔 很遗憾，没有检索到相应关键字的资源，请更换关键字再次检索，也可以尝试联系我们的设计团队成员。</li>";
+  } else {
+    softwareList.forEach((software) => {
+      const listItem = document.createElement("li");
+
+      const thumbnail = document.createElement("img");
+      thumbnail.src = software.thumbnail;
+      thumbnail.alt = software.name + "缩略图";
+      thumbnail.classList.add("software-thumbnail");
+
+      const nameText = document.createElement("span");
+      nameText.textContent = software.name;
+
+      listItem.appendChild(thumbnail);
+      listItem.appendChild(nameText);
+
+      listItem.onclick = () => {
+        window.location.href = software.page;
+      };
+
+      listElement.appendChild(listItem);
+    });
+  }
+}
+
+// 通用的加载模块函数
+function loadModule(file, targetId, callback) {
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(targetId).innerHTML = data;
+      if (callback) callback();  // 加载后回调
+    })
+    .catch(err => console.error(`加载 ${file} 失败：`, err));
+}
+
+// 加载 header，并在加载完后绑定 header 内的事件
+loadModule('header.html', 'header', () => {
+  const searchInput = document.getElementById("searchInput");
+  if (searchInput) {
+    searchInput.addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        searchSoftware();
+      }
+    });
+
+    const searchButton = document.querySelector('.search button');
+    if (searchButton) {
+      searchButton.addEventListener('click', searchSoftware);
+    }
+  }
+});
+
+// 加载 footer
+loadModule('footer.html', 'footer');
+
+// 监听页面加载，处理搜索结果
+window.addEventListener('load', handleSearchResults);
